ad45c6255f93bae0aae87a237e1c2b0f778d3f4e
Guy Daughtrey <guyddv@users.noreply.github.com>
2021-08-22 20:55:04 -0500

    Implement switch statement and some error checks.

--- Calculator-Console-App/Program.cs
+++ Calculator-Console-App/Program.cs
@@ -6,26 +6,52 @@ namespace Calculator_Console_App
     {
         static void Main(string[] args)
         {
-            string firstNumberEntered = "";
-            string secondNumberEntered = "";
-            string operationEntered;
-            int result;
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int result = 0;
 
             Console.WriteLine("Please enter the first number: ");
-            firstNumberEntered = Console.ReadLine();
+            string firstNumberEntered = Console.ReadLine();
+            if (Convert.ToInt32(firstNumberEntered) is int)
+            {
+                firstNumber = Convert.ToInt32(firstNumberEntered);
+            }
+            else
+            { 
+                Console.WriteLine("Please enter a valid integer number.");
+            }
+
             Console.WriteLine("Please enter the second number: ");
-            secondNumberEntered = Console.ReadLine();
+            string secondNumberEntered = Console.ReadLine();
+            if (Convert.ToInt32(secondNumberEntered) is int)
+            {
+                secondNumber = Convert.ToInt32(secondNumberEntered);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid integer number.");
+            }
+            
             Console.WriteLine("Please enter the operation you would like to perform (+, -, *, /): ");
-            operationEntered = Console.ReadLine();
+            string operationEntered = Console.ReadLine();
 
             switch (operationEntered) {
                 case "+":
-                    int firstNumber = Convert.ToInt32(firstNumberEntered);
-                    int secondNumber = Convert.ToInt32(secondNumberEntered);
                     result = firstNumber + secondNumber;
                     Console.WriteLine($"{firstNumberEntered} + {secondNumberEntered} = {result}");
                     break;
-
+                case "-":
+                    result = firstNumber - secondNumber;
+                    Console.WriteLine($"{firstNumber} - {secondNumber} = {result}");
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    Console.WriteLine($"{firstNumber} * {secondNumber} = {result}");
+                    break;
+                case "/":
+                    result = firstNumber / secondNumber;
+                    Console.WriteLine($"{firstNumber} / {secondNumber} = {result}");
+                    break;
                 default:
                     Console.WriteLine("Please enter +, -, * or /");
                     break;
